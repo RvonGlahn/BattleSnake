60ee2051d39b430bb2adbf918f94bb7b1bc5468d
RvonGlahn <rasmusvong@gmx.de>
2020-12-15 16:38:46 +0100

    added escapeplan plan

--- agents/heuristics/EcapePlan.py
+++ agents/heuristics/EcapePlan.py
@@ -1,4 +1,26 @@
+import numpy as np
+import numba
 
-# Floodfill
-# Turn around possible
-# Fluchtwege müssen immer U-turn ermöglichen
+
+class EscapePlan:
+
+    def __init__(self, width, height, grid_map):
+        self.value_board = np.zeros((width, height))
+        self._calculate_value_board(grid_map)
+
+    def _get_snake_radius(self):
+        pass
+
+    def _calculate_value_board(self,grid_map):
+        pass
+
+    def _evaluate_corridor(self):
+        # width, length, snake_heads near corridor
+        pass
+
+    def add_relevant_snake(self, snake, snake_automat):
+        pass
+
+    def escape(self):
+        pass
+
